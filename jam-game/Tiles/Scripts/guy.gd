9d526d63aa24cb807e4c16123b76ebbb75c01a03
octokid12 <andrew.sargent2021@gmail.com>
2026-02-05 19:16:40 -0500

    this  may break something

--- jam-game/Tiles/Scripts/guy.gd
+++ jam-game/Tiles/Scripts/guy.gd
@@ -1,10 +1,13 @@
 extends TileMapLayer
 var selfpos: Vector2
 
+const one = Vector2i(0, 0)
+const two = Vector2i(1, 0)
+const three = Vector2i(2, 0)
+
+
 
 func _physics_process(delta: float) -> void:
 	clear()
-	set_cell(Global.Payloadpos, 1, Vector2i(2, 3))
-
-
-		
+	set_cell(Global.Payloadpos, 2, one)
+	
